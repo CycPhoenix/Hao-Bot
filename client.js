46f87c68a550e9f5f69016aea130bee42e8641b5
CycPhoenix <cycphoenixgt@gmail.com>
2021-07-31 23:27:16 +0800

    Updated client.js

--- client.js
+++ client.js
@@ -2,11 +2,20 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 const config = require('./config.json')
+const command = require('./command')
 
 client.on('ready', async () => {
     console.log(`${client.user.username} Is Online!`)
 
     client.user.setActivity('On Development', { type: 'PLAYING' })
+
+    command(client, 'vote', (message) => {
+        const { member, mentions } = message
+
+        const tag = `<@${member.id}>`
+
+        message.reply('Vote Link:\nhttps://docs.google.com/forms/d/e/1FAIpQLSd6CYW2z6Tz76nF4nk4zLR3Gd598H2d-ReLSDHyLi8UtFTsLA/viewform?usp=sf_link')
+    })
 })
 
-client.login(process.env.token)
+client.login(config.token)
